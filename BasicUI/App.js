7a6d3e7c0357e3cfe4529021b7da1fd5f021305b
fourthedition <48860054+fourthedition@users.noreply.github.com>
2020-10-04 19:24:14 +0700

    basic

---
+++ BasicUI/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.bg}>
+        <Text style={{color:'white', fontWeight:'bold', paddingLeft:10}}>WELCOME TO</Text>
+        <Text style={{color:'white', fontWeight:'bold', paddingLeft:10, fontSize:25}}>COVID-19</Text>
+        <Text style={{color:'white', fontWeight:'bold', paddingLeft:10, fontSize:25}}>NEW NORMAL TH</Text>
+        <Text style={{color:'white', fontWeight:'bold', padding:10, fontSize:10}}>Update 2020-10-04</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b7b7ff',
+  },
+  bg: {
+    flex: 0.25,
+    backgroundColor: '#436bfc',
+    borderBottomLeftRadius:30,
+    borderBottomRightRadius:30,
+    justifyContent:'center',
+  }
+});
