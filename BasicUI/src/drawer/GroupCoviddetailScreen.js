a3e6cb0ced3fd739393b81e93414972bebe22925
fourthedition <48860054+fourthedition@users.noreply.github.com>
2020-10-12 21:14:03 +0700

    ui

--- BasicUI/src/drawer/GroupCoviddetailScreen.js
+++ BasicUI/src/drawer/GroupCoviddetailScreen.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
-import {
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-  StyleSheet,
-} from 'react-native';
-import {CustomHeader} from '../index';
-import Color from '../constants/colors';
-import {IMAGE} from '../constants/image';
+import React, { Component } from 'react';
+import { Text, View, SafeAreaView,Image, TouchableOpacity, ScrollView } from 'react-native';
+import {CustomHeader} from '../index'
 
-export class GroupCoviddetailScreen extends Component {
-  render() {
-  }
+
+export class GroupCoviddetailScreen extends Component{
+    render(){
+        return (
+            <SafeAreaView style={{ flex: 1}}>
+              <CustomHeader title='มาตรการระดับบุคคล' navigation={this.props.navigation}/>
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text> กลุ่มเสี่ยง Screen</Text>
+              </View>
+            </SafeAreaView>
+          );
+    }
 }
